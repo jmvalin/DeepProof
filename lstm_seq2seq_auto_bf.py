85ae460e7db64fbbc047af4ff58f279c7c13ed3b
Jean-Marc Valin <jmvalin@jmvalin.ca>
2017-12-02 22:52:31 -0500

    correcting substitutions

--- lstm_seq2seq_auto_bf.py
+++ lstm_seq2seq_auto_bf.py
@@ -58,16 +58,16 @@ import numpy as np
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.45
+config.gpu_options.per_process_gpu_memory_fraction = 0.29
 set_session(tf.Session(config=config))
 
 
 batch_size = 64  # Batch size for training.
 epochs = 300  # Number of epochs to train for.
-latent_dim = 1024  # Latent dimensionality of the encoding space.
+latent_dim = 512  # Latent dimensionality of the encoding space.
 num_samples = 500000  # Number of samples to train on.
 # Path to the data txt file on disk.
-data_path = 'training.txt'
+data_path = 'training2.txt'
 
 # Vectorize the data.
 input_texts = []
@@ -92,41 +92,33 @@ for line in lines[: min(num_samples, len(lines) - 1)]:
 input_characters = sorted(list(input_characters))
 target_characters = sorted(list(target_characters))
 num_encoder_tokens = len(input_characters)
-num_decoder_tokens = len(target_characters)
 max_encoder_seq_length = max([len(txt) for txt in input_texts])
 max_decoder_seq_length = max([len(txt) for txt in target_texts])
 
 print('Number of samples:', len(input_texts))
 print('Number of unique input tokens:', num_encoder_tokens)
-print('Number of unique output tokens:', num_decoder_tokens)
 print('Max sequence length for inputs:', max_encoder_seq_length)
 print('Max sequence length for outputs:', max_decoder_seq_length)
 
-input_token_index = dict(
-    [(char, i) for i, char in enumerate(input_characters)])
 target_token_index = dict(
-    [(char, i) for i, char in enumerate(target_characters)])
+    [(char, i) for i, char in enumerate(input_characters)])
 
 encoder_input_data = np.zeros(
     (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
     dtype='int8')
 decoder_input_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    (len(input_texts), max_decoder_seq_length, num_encoder_tokens),
     dtype='int8')
 decoder_target_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    (len(input_texts), max_decoder_seq_length, num_encoder_tokens),
     dtype='int8')
 
 for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
     for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.
+        encoder_input_data[i, t, target_token_index[char]] = 1.
     for t, char in enumerate(target_text):
         # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+        decoder_target_data[i, t, target_token_index[char]] = 1.
 
 # Define an input sequence and process it.
 encoder_inputs = Input(shape=(None, num_encoder_tokens))
@@ -136,14 +128,14 @@ encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 encoder_states = [state_h, state_c]
 
 # Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_inputs = Input(shape=(None, num_encoder_tokens))
 # We set up our decoder to return full output sequences,
 # and to return internal states as well. We don't use the
 # return states in the training model, but we will use them in inference.
 decoder_lstm = CuDNNLSTM(latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                      initial_state=encoder_states)
-decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_dense = Dense(num_encoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
 # Define the model that will turn
@@ -151,8 +143,8 @@ decoder_outputs = decoder_dense(decoder_outputs)
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 # Run training
-model.compile(optimizer='adam', loss='categorical_crossentropy')
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit([encoder_input_data, encoder_input_data], decoder_target_data,
           batch_size=batch_size,
           epochs=epochs,
           validation_split=0.2)
@@ -184,7 +176,7 @@ decoder_model = Model(
 # Reverse-lookup token index to decode sequences back to
 # something readable.
 reverse_input_char_index = dict(
-    (i, char) for char, i in input_token_index.items())
+    (i, char) for char, i in target_token_index.items())
 reverse_target_char_index = dict(
     (i, char) for char, i in target_token_index.items())
 
@@ -194,7 +186,7 @@ def decode_sequence(input_seq):
     states_value = encoder_model.predict(input_seq)
 
     # Generate empty target sequence of length 1.
-    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    target_seq = np.zeros((1, 1, num_encoder_tokens))
     # Populate the first character of target sequence with the start character.
     target_seq[0, 0, target_token_index['\t']] = 1.
 
@@ -218,7 +210,7 @@ def decode_sequence(input_seq):
             stop_condition = True
 
         # Update the target sequence (of length 1).
-        target_seq = np.zeros((1, 1, num_decoder_tokens))
+        target_seq = np.zeros((1, 1, num_encoder_tokens))
         target_seq[0, 0, sampled_token_index] = 1.
 
         # Update states
--- process_sentences.c
+++ process_sentences.c
@@ -14,11 +14,16 @@ int main()
     //printf ("%d %d %d %d\n", len, alloc_size, (int)line[len], (int)line[len-1]);
     if (len < 100) {
       int i;
+      char corrupt[110];
       for (i=0;i<len;i++) {
         if (line[i] < 32 || line[i] > 126)
           line[i] = 95;
       }
-      printf("%s\t%s\n", line, line);
+      strncpy(corrupt, line, 100);
+      for (i=0;i<len;i++) {
+        if (rand()%50==0) corrupt[i] = 32 + rand()%95;
+      }
+      printf("%s\t%s\n", corrupt, line);
     }
   }
   return 0;
